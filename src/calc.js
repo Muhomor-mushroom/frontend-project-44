faa5d78682ff9c24290d3e74bc0ad9d8e0dbbec5
AX <ax@MacBook-Pro-NS.local>
2024-09-03 11:03:14 +0300

    Calculator

--- src/calc.js
+++ src/calc.js
@@ -1,36 +1,47 @@
-import readlineSync from 'readline-sync';
+import readlineSync from "readline-sync";
+import resulter from "../src/index.js";
 
 const secondGame = () => {
-    const name = readlineSync.question('May I have your name? ');
-    console.log(`Hello, ${name}!`)
+    const name = readlineSync.question("May I have your name? ");
+    console.log(`Hello, ${name}!`);
     console.log('What is the result of the expression?');
-    let counter = 0;
-    let isTrue;
-    const question = () => {
-        const firstRandomNumber = Math.floor((Math.random() * 8) + 1);
-        const secondRandomNumber = Math.floor((Math.random() * 8) + 1);
-        const result = firstRandomNumber + secondRandomNumber;
-        console.log(`Question: ${firstRandomNumber} + ${secondRandomNumber}`)
-        const answer = readlineSync.question('Your answer: ');
-        if (answer == result) {
-            console.log(`Correct!`);
-            isTrue = true;
-            counter += 1;
-        } else {
-            console.log(`'${answer}' is wrong answer ;(. Correct answer was '${result}'.`)
-            console.log(`Let's try again, ${name}!`)
-            isTrue = false;
-        }
+
+    for (let i = 1; i <= 3; i += 1) {
+        let firstRandomNumber = Math.floor(
+            Math.random() * (Math.floor(100) - Math.ceil(1))
+          );
+        let secondRandomNumber = Math.floor(
+            Math.random() * (Math.floor(100) - Math.ceil(1))
+          );
+        let randomOperand = Math.floor(Math.random() * ((Math.floor(4) - Math.ceil(1)) + Math.ceil(1)));
+
+        let correctAnswer;
+
+          switch(randomOperand) {
+            case 1:
+                correctAnswer = firstRandomNumber + secondRandomNumber;
+                console.log(`Question: ${firstRandomNumber} + ${secondRandomNumber}`);
+                break;
+            case 2:
+                correctAnswer = firstRandomNumber - secondRandomNumber;
+                console.log(`Question: ${firstRandomNumber} - ${secondRandomNumber}`);
+                break;
+            default:
+                correctAnswer = firstRandomNumber * secondRandomNumber;
+                console.log(`Question: ${firstRandomNumber} * ${secondRandomNumber}`);
+                break;
+          }
+          let answer = readlineSync.question("Your answer: ");
+          if (answer !== correctAnswer.toString()) {
+            console.log(
+                `'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`
+              );
+              resulter("incorrect", name);
+              return;
+          }
+          resulter("correct");
     }
-    question()
-    if (isTrue == true) {
-        question();
-        if (isTrue == true) {
-            question()
-        }
-    }
-    if (counter == 3) {
-        console.log(`Congratulations, ${name}!`)
-    }
+    console.log(`Congratulations, ${name}!`);
 }
-export default secondGame
+secondGame();
+export default secondGame;
--- src/isEven.js
+++ src/isEven.js
@@ -1,37 +1,29 @@
-import readlineSync from 'readline-sync';
-import resulter from '../src/index.js';
+import readlineSync from "readline-sync";
+import resulter from "../src/index.js";
 
 const firstGame = () => {
-    const name = readlineSync.question('May I have your name? ');
-    console.log('Answer "yes" if the number is even, otherwise answer "no".');
-    
-    const isEven = (number) => {
-        if (number % 2 == 0) {
-            return true
-        } return false
-    };
+  const name = readlineSync.question("May I have your name? ");
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
 
-    for (let i = 1; i <= 3; i += 1) {
-        let randomNumber = Math.floor(Math.random() * (Math.floor(500) - Math.ceil(1)));
-        console.log(`Question: ${randomNumber}`);
-        const answer = readlineSync.question('Your answer: ');
-        
-        if (isEven(randomNumber) && answer == 'yes' || !isEven(randomNumber) && answer == 'no') {
-            resulter('correct');
-        } else if (isEven(randomNumber) && answer == 'no') {
-            console.log("Answer 'no' is wrong answer ;(. Correct answer was 'yes'.");
-            resulter('no', name)
-            break;
-        } else {
-            console.log("Answer 'yes' if the number is even, otherwise answer 'no'.")
-            resulter('yes', name);
-            break;
-        };
-
-        if (i == 3) {
-            console.log(`Congratulations, ${name}!`);
-            break;
-        }
+  const isEven = (number) => {
+    if (number % 2 === 0) {
+      return 'yes';
     }
-}
-export default firstGame
+    return 'no';
+  };
+
+  for (let i = 1; i <= 3; i += 1) {
+    const randomNumber = Math.floor(Math.random() * (Math.floor(500) - Math.ceil(1)));
+    console.log(`Question: ${randomNumber}`);
+    const answer = readlineSync.question("Your answer: ");
+    const correctAnswer = isEven(randomNumber);
+    if (answer !== correctAnswer) {
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+      resulter("incorrect", name);
+      return;
+    }
+    resulter("correct");
+  }
+  console.log(`Congratulations, ${name}!`);
+};
+export default firstGame;
