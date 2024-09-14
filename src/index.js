bdec940fc518a1ad78182a1e26b94febbe569464
AX <ax@MacBook-Pro-NS.local>
2024-09-14 11:21:05 +0300

    second-mistake

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const resulter = (endResult, name) => {
   if (endResult === 'correct') {
     console.log('Correct!');
   } else {
-    console.log(`Let's try again, ${name}`);
+    console.log(`Let's try again, ${name}!`);
   }
 };
 
